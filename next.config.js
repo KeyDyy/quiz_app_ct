cb5cf4801c997d9e9bc37c62cc4941e6e2ba11bf
Konrad <konrad.kornak9@gmail.com>
2025-05-22 22:06:11 +0200

    desp5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_TENANT_ID: process.env.NEXT_PUBLIC_TENANT_ID,
+    },
 }
 
 module.exports = nextConfig
